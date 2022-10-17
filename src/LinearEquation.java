9c864612611f2f7bea4740524de28a97f6606c41
1E10 Laptop <sample@bths.edu>
2022-10-17 12:33:21 -0400

    commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private double x1;
+    private double x2;
+    private double y1;
+    private double y2;
+
+    public LinearEquation(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double calculateSlope(double x1, double y1, double x2, double y2) {
+        return (y2 - y1) / (x2 - x1);
+    }
+
+    public double calculateIntercept() {
+        return y2 - (x1 * calculateSlope(x1, y1, x2, y2));
+    }
+
+    public double roundedToHundredth(double num) {
+        return Math.round(num);
+    }
+
+    public String returnEquation() {
+        int temp = (int) calculateSlope(x1, y1, x2, y2);
+        if (calculateSlope(x1, y1, x2, y2) > temp) {
+            if (calculateSlope(x1, y1, x2, y2) < 0) {
+                return "y = " + "-" + Math.abs(y2 - y1) + "/" + Math.abs(x2 - x1) + "x  + " + calculateIntercept();
+            }
+            return "y = " + (y2 - y1) + "/" + (x2 - x1) + "x  + " + calculateIntercept();
+        }
+        if (calculateSlope(x1, y1, x2, y2) == 0) {
+            return "y = " + calculateIntercept();
+        }
+        if (calculateSlope(x1, y1, x2, y2) < 0) {
+            return "y = " + "-" + Math.abs(calculateSlope(x1,y1,x2,y2) + "x + " + calculateIntercept();
+        }
+        return "y = " + calculateSlope(x1, y1, x2, y2) + "x + " + calculateIntercept();
+    }
+
+
+
+
+
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import java.util.Scanner;
+public class LinearEquationRunner {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Welcome!");
+    System.out.println("Enter coordinate 1: ");
+    String coords1 = scan.nextLine();
+    String withoutPar = coords1.substring(1).substring(0, coords1.length() - 1);
+    int indexOfComma = coords1.indexOf(",");
+    double x1 = Integer.parseInt(withoutPar.substring(0, indexOfComma));
+    double y1 = Integer.parseInt(withoutPar.substring(indexOfComma + 2));
+    System.out.println("Enter coordinate 1: ");
+    String coords2 = scan.nextLine();
+    double x2 = Integer.parseInt( coords1.substring(1,2));
 }
