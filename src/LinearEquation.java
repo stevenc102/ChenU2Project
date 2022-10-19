bc4f734362df6682e30e65f47e67ac492d939654
Steven <stevenc102@nycstudents.net>
2022-10-19 19:40:32 -0400

    commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -16,32 +16,91 @@ public class LinearEquation {
     }
 
     public double calculateIntercept() {
-        return y2 - (x1 * calculateSlope(x1, y1, x2, y2));
+        return roundedToHundredth((y2 - (x2 * calculateSlope(x1, y1, x2, y2))));
     }
 
     public double roundedToHundredth(double num) {
-        return Math.round(num);
+        return Math.round(num * 100) / 100.0;
+    }
+
+    public double calculateDistance() {
+        return roundedToHundredth(Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
     }
 
     public String returnEquation() {
-        int temp = (int) calculateSlope(x1, y1, x2, y2);
-        if (calculateSlope(x1, y1, x2, y2) > temp) {
-            if (calculateSlope(x1, y1, x2, y2) < 0) {
-                return "y = " + "-" + Math.abs(y2 - y1) + "/" + Math.abs(x2 - x1) + "x  + " + calculateIntercept();
+        String interceptAsString = "" + calculateIntercept();
+        boolean isNegative = (calculateIntercept() < 0);
+        boolean isNegativeSlope = (calculateSlope(x1,y1,x2,y2) < 0);
+        int temp = (int) calculateSlope(x1,y1,x2,y2);
+        boolean isDecimal = (calculateSlope(x1,y1,x2,y2) > temp || calculateSlope(x1,y1,x2,y2) < temp);
+        String y = "" + (int) (y2 - y1);
+        String x = "" + (int) (x2 - x1);
+        if (calculateSlope(x1,y1,x2,y2) == 0) {
+            return "y = " + (int)calculateIntercept();
+        }
+        if (calculateSlope(x1,y1,x2,y2) == 1) {
+            if (isNegative) {
+                if (calculateIntercept() == 0) {
+                    return "y = x" + calculateSlope(x1,y1,x2,y2) + "x";
+                }
+                return "y = x" + " - " + interceptAsString.substring(1);
             }
-            return "y = " + (y2 - y1) + "/" + (x2 - x1) + "x  + " + calculateIntercept();
+            if (calculateIntercept() == 0) {
+                return "y = x";
+            }
+            return "y = x" + " + " + interceptAsString.substring(1);
         }
-        if (calculateSlope(x1, y1, x2, y2) == 0) {
-            return "y = " + calculateIntercept();
+        if (calculateSlope(x1,y1,x2,y2) == -1) {
+            if (isNegative) {
+                if (calculateIntercept() == 0) {
+                    return "y = -x" + calculateSlope(x1,y1,x2,y2) + "x";
+                }
+                return "y = -x" + " - " + interceptAsString.substring(1);
+            }
+            if (calculateIntercept() == 0) {
+                return "y = -x";
+            }
+            return "y = -x" + " + " + interceptAsString.substring(1);
         }
-        if (calculateSlope(x1, y1, x2, y2) < 0) {
-            return "y = " + "-" + Math.abs(calculateSlope(x1,y1,x2,y2) + "x + " + calculateIntercept();
+        if (calculateIntercept() == 0) {
+            if (isDecimal) {
+                if (y2 - y1 > 0 || x2 - x1 > 0) {
+                    if (isNegative) {
+                        if (isNegativeSlope) {
+                            return "y = -" + Math.abs((int) (y2 - y1)) + "/" + Math.abs((int) (x2 - x1)) + "x";
+                        }
+                        return "y = " + Math.abs((int) (y2 - y1)) + "/" + Math.abs((int) (x2 - x1)) + "x";
+                    }
+                    if (isNegativeSlope) {
+                        return "y = -" + Math.abs((int) (y2 - y1)) + "/" + Math.abs((int) (x2 - x1)) + "x";
+                    }
+                    return "y = " + Math.abs((int) (y2 - y1)) + "/" + Math.abs((int) (x2 - x1)) + "x";
+                }
+
+            }
+            return "y = " + calculateSlope(x1,y1,x2,y2) + "x";
         }
-        return "y = " + calculateSlope(x1, y1, x2, y2) + "x + " + calculateIntercept();
+        if (isDecimal) {
+            if (y2 - y1 > 0 || x2 - x1 > 0) {
+                if (isNegative) {
+                    if (isNegativeSlope) {
+                        return "y = -" + Math.abs((int) (y2 - y1)) + "/" + Math.abs((int) (x2 - x1)) + "x" + " - " + interceptAsString.substring(1);
+                    }
+                    return "y = " + Math.abs((int) (y2 - y1)) + "/" + Math.abs((int) (x2 - x1)) + "x" + " - " + interceptAsString.substring(1);
+                }
+                if (isNegativeSlope) {
+                    return "y = -" + Math.abs((int) (y2 - y1)) + "/" + Math.abs((int) (x2 - x1)) + "x" + " + " + interceptAsString;
+                }
+                return "y = " + Math.abs((int) (y2 - y1)) + "/" + Math.abs((int) (x2 - x1)) + "x" + " + " + interceptAsString.substring(1);
+            }
+            if (isNegative) {
+                return "y = " + y.substring(1) + "/" + x.substring(1) + "x" + " - " + interceptAsString.substring(1);
+            }
+            return "y = " + y.substring(1) + "/" + x.substring(1) + "x" + " + " + interceptAsString;
+        }
+        if (isNegative) {
+            return "y = " + (int) calculateSlope(x1,y1,x2,y2) + "x" + " - " + interceptAsString.substring(1);
+        }
+        return "y = " + (int) calculateSlope(x1,y1,x2,y2) + "x" + " + " + interceptAsString;
     }
-
-
-
-
-
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,15 +1,53 @@
 import java.util.Scanner;
 public class LinearEquationRunner {
-    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-    System.out.println("Welcome!");
-    System.out.println("Enter coordinate 1: ");
-    String coords1 = scan.nextLine();
-    String withoutPar = coords1.substring(1).substring(0, coords1.length() - 1);
-    int indexOfComma = coords1.indexOf(",");
-    double x1 = Integer.parseInt(withoutPar.substring(0, indexOfComma));
-    double y1 = Integer.parseInt(withoutPar.substring(indexOfComma + 2));
-    System.out.println("Enter coordinate 1: ");
-    String coords2 = scan.nextLine();
-    double x2 = Integer.parseInt( coords1.substring(1,2));
+        System.out.println("Welcome!");
+        System.out.print("Enter coordinate 1: ");
+
+
+        String coords1 = scan.nextLine();
+        int indexOfClosing1 = coords1.indexOf(")");
+        String withoutPar = coords1.substring(1).substring(0, indexOfClosing1 - 1);
+        int indexOfComma = coords1.indexOf(",");
+
+        double x1 = Integer.parseInt(withoutPar.substring(0, indexOfComma - 1));
+        double y1 = Integer.parseInt(withoutPar.substring(indexOfComma + 1));
+
+        System.out.print("Enter coordinate 2: ");
+        String coords2 = scan.nextLine();
+        int indexOfClosing2 = coords2.indexOf(")");
+        String withoutPar2 = coords2.substring(1).substring(0, indexOfClosing2 - 1);
+        int indexOfComma2 = coords2.indexOf(",");
+
+
+        double x2 = Integer.parseInt(withoutPar2.substring(0, indexOfComma2 - 1));
+        double y2 = Integer.parseInt(withoutPar2.substring(indexOfComma2 + 1));
+
+        System.out.println("");
+
+        LinearEquation linearEq = new LinearEquation(x1,y1,x2,y2);
+
+        if (x2 - x1 != 0) {
+            System.out.println("The two points are: (" + (int) x1 + ", " + (int) y1 + ")" + " and " + "(" + (int) x2 + ", " + (int) y2 + ")");
+            System.out.println("The equation of the line between these points is " + linearEq.returnEquation());
+            System.out.println("The slope of this line is: " + linearEq.roundedToHundredth(linearEq.calculateSlope(x1, y1, x2, y2)));
+            double slope = linearEq.calculateSlope(x1, y1, x2, y2);
+            System.out.println("The y-intercept of the line is: " + linearEq.calculateIntercept());
+            System.out.println("The distance between the two points is: " + linearEq.calculateDistance());
+
+            System.out.println("");
+
+            System.out.print("Enter a value for x: ");
+            double newX = scan.nextDouble();
+            scan.nextLine();
+            System.out.println("\nThe point on the line is (" + newX + ", " + (slope * newX + (y2 - (x2 * linearEq.calculateSlope(x1, y1, x2, y2))) + ")"));
+
+        } else {
+
+            System.out.println("These points are on a vertical line: " + "x = " + x1);
+        }
+
+    }
 }
